7cc4d89abe0630cdd6ed17f50d4174f22ba2fcb9
tkhwang <tkhwang@gmail.com>
2019-03-22 15:25:37 +0900

    [TEST] EC2 IP change applied.

--- deploy-build.sh
+++ deploy-build.sh
@@ -3,17 +3,22 @@
 # EC2 was already setup. CI/CD gets update and rebuild it.
 echo "[+] Start deploy-build."
 
+echo "[+] Move to studystates-server folder/dev"
 cd /home/ubuntu/studystates-server/dev
 
 # Decompress source code
-tar -zxvf dev.tar.gz
+echo "[+] Decompress source file"
+tar -zxvf studystates-server.tar.gz
 
 # Install package
+echo "[+] Install node package files"
 npm install
 
 # Server build including typescript
+echo "[+] Run build"
 npm run build
 
 # Start server
+echo "[+] Start server"
 pm2 start npm --name "studystates-server" -- start
 echo "[+] End deploy-build."
