c3d1c6193428e9f53a6ad4b25858e29a4691eb3b
dahaekim88 <dhkim1014@gmail.com>
2019-04-18 17:23:05 +0900

    Fix response at payment handler

--- src/util/iamport.ts
+++ src/util/iamport.ts
@@ -1,6 +1,7 @@
+// TODO: WHY NOT WORKING?
 // import { IMP_KEY, IMP_SECRET } from "../util/secrets";
-const imp_key = "0794151887787607";
-const imp_secret = "sOaZXYF3GFtPfAJ6nvW9a0S1mRkyx0PZO0ZMZFQFkjvAUeGhKCqUDjVZagyIhoI6oHTnnJ3I2yiWNZex";
+const IMP_KEY = "0794151887787607";
+const IMP_SECRET = "sOaZXYF3GFtPfAJ6nvW9a0S1mRkyx0PZO0ZMZFQFkjvAUeGhKCqUDjVZagyIhoI6oHTnnJ3I2yiWNZex";
 const IMP_URL = "https://api.iamport.kr";
 import axios from "axios";
 
@@ -8,8 +9,8 @@ export const getAccessToken = async () => {
   // console.log("IMP_KEY: ", IMP_KEY);
   // console.log("IMP_SECRET: ", IMP_SECRET);
   const getToken = await axios.post(`${IMP_URL}/users/getToken`, {
-    imp_key,
-    imp_secret
+    IMP_KEY,
+    IMP_SECRET
   });
   const accessToken = getToken.data.response.access_token;
   return {
