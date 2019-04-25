c20474d3885bc2e485950061980490bcea1ac384
dahaekim88 <dhkim1014@gmail.com>
2019-04-25 13:44:21 +0900

    Handle filtering data with user email

--- src/controllers/mypage.ts
+++ src/controllers/mypage.ts
@@ -17,13 +17,12 @@ export const getCourses = async (req: Request, res: Response) => {
     const decoded = jwtDecode(token) as { id: number };
     const id = decoded.id;
     // console.log("id: ", id);
-    const user = await getRepository(User).findOne({ id });
 
     const paymentQb = await getRepository(Payment).createQueryBuilder("payment");
 
     const result = await paymentQb
       .select("payment.study_id")
-      .where("payment.userId = :userId", { userId: id }) // TODO: userId 가능한지 다시 확인! user 넣을 경우 syntax 에러
+      .where("payment.userId = :userId", { userId: id })
       .andWhere("payment.status = :status", { status: "paid" })
       .orderBy("payment.paid_at", "ASC")
       .getMany();
@@ -44,12 +43,17 @@ export const getMembers = async (req: Request, res: Response) => {
   try {
     const study_id = req.query.study_id;
 
+    const token = req.headers["x-auth-token"] as string;
+    const decoded = jwtDecode(token) as { id: number };
+    const id = decoded.id;
+
     const userQb = await getRepository(User).createQueryBuilder("user");
     const members = await userQb
       .innerJoin("user.payment", "payment")
       .select(["user.id", "user.name", "user.email", "user.phone"])
       .where("payment.status = :status", { status: "paid" })
       .andWhere("payment.study_id = :study_id", { study_id })
+      .andWhere("user.id != :id", { id })
       .getMany();
 
     res.json({
